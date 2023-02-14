c101e6958ec96ee9f93291828d616f296ff32ba1
Alex Book <book.45@osu.edu>
2023-02-14 10:01:34 -0500

    update fields of Movie and change dummydataservice to use our movie jsons

--- rater-backend/backend/src/main/java/rater/application/model/Movie.java
+++ rater-backend/backend/src/main/java/rater/application/model/Movie.java
@@ -1,9 +1,20 @@
 package rater.application.model;
 
+import java.util.List;
+import java.util.Map;
+
 public class Movie {
 
     private String id;
     private String title;
+    private List<String> starring;
+    private int runtime; //minutes
+    private List<String> genre;
+    private String description;
+    private int releaseYear;
+    private String rating; //G, PG, etc.
+    private String image;
+    private Map<String, Integer> categories;
     
     public String getId() {
         return id;
--- rater-backend/backend/src/main/java/rater/application/service/helper/MovieDummyDataService.java
+++ rater-backend/backend/src/main/java/rater/application/service/helper/MovieDummyDataService.java
@@ -1,10 +1,13 @@
 package rater.application.service.helper;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import rater.application.model.Movie;
 import rater.application.service.MovieService;
@@ -12,6 +15,7 @@ import rater.application.service.MovieService;
 @Service
 public class MovieDummyDataService {
     private static final Logger LOG = LoggerFactory.getLogger(MovieDummyDataService.class);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
     private final MovieService movieService;
@@ -21,15 +25,22 @@ public class MovieDummyDataService {
     }
 
     public void insertDummyData() {
-        movieService.index(buildMovie("1", "Raiders"));
-        movieService.index(buildMovie("2", "Blade Runner 2049"));
+        File jsonDirectory = new File(System.getProperty("user.dir") + "/src/main/resources/static/documents");
+        File[] jsonFiles = jsonDirectory.listFiles();
+        for (File jsonDoc: jsonFiles) {
+            movieService.index(buildMovie(jsonDoc));
+        }
     }
 
-    private static Movie buildMovie(final String id, final String title) {
-        Movie movie = new Movie();
-        movie.setId(id);
-        movie.setTitle(title);
-
-        return movie;
+    private static Movie buildMovie(File jsonDoc) {
+        try {
+            Movie result = MAPPER.readValue(jsonDoc, Movie.class);
+            return result;
+        } catch (Exception e) {
+            LOG.error("Error building movie from JSON document");
+            LOG.error(e.getMessage());
+            return null;
+        }
+        
     }
 }
