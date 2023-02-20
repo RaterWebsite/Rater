2bc0ebb7c3a2fbd6195a2920d59fc453fdf6132c
Andrew Lewandowski <48466309+andylewi@users.noreply.github.com>
2023-02-20 15:58:30 -0500

    Added functions to create movie data items

---
+++ rater-frontend/src/functions.js
@@ -0,0 +1,92 @@
+function getProgressColor(percent){
+  let r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+  let g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
+  return 'rgb('+r+','+g+',0)';
+}
+
+function getTimeString(minutes){
+  let message = ""
+  if(minutes >= 60){
+    message+= Math.floor(minutes/60) + 'h '
+    minutes = minutes % 60
+  }
+  if(minutes > 0){
+    message+= minutes +'m'
+  }
+  return message
+}
+
+function generateItem(movieData, categories, weights){
+  const containerDiv = document.createElement("div")
+  containerDiv.className = 'container'
+
+  const leftDiv = document.createElement("div")
+  leftDiv.className = 'left-container'
+
+  const middleDiv = document.createElement("div")
+  middleDiv.className = 'middle-container'
+
+  const rightDiv = document.createElement("div")
+  rightDiv.className = 'right-container'
+
+  containerDiv.appendChild(leftDiv)
+  containerDiv.appendChild(middleDiv)
+  containerDiv.appendChild(rightDiv)
+
+  const imageDiv = document.createElement("img")
+  imageDiv.className = 'movie-image'
+  imageDiv.src = movieData['image']
+  leftDiv.appendChild(imageDiv)
+
+  const titleDiv = document.createElement("div")
+  titleDiv.className = 'title-item'
+  titleDiv.style.fontSize = "32px";
+  titleDiv.innerHTML = movieData['title'] + ' (' + movieData['releaseYear'] + ')'
+  middleDiv.appendChild(titleDiv)
+
+  const infoDiv = document.createElement("div")
+  infoDiv.className = 'info-item'
+  infoDiv.innerHTML = movieData['releaseYear'] + ' • ' + movieData['rating'] + ' • ' + getTimeString(movieData['runtime'])
+  middleDiv.appendChild(infoDiv)
+
+  const descDiv = document.createElement("div")
+  descDiv.className = 'desc-item'
+  descDiv.innerHTML = movieData['description']
+  middleDiv.appendChild(descDiv)
+
+  const genreDiv = document.createElement("div")
+  genreDiv.className = 'genre-item'
+  genreDiv.innerHTML = movieData['genre'].join(' • ')
+  middleDiv.appendChild(genreDiv)
+
+  const starringDiv = document.createElement("div")
+  starringDiv.className = 'starring-item'
+  starringDiv.innerHTML = movieData['starring'].join(' • ')
+  middleDiv.appendChild(starringDiv)
+
+
+  var overallTotal = 0
+  var overallOutOf = 0
+  for(let i = 0; i < categories.length; i++){
+    let categoryName = categories[i]
+    categories[i] = categories[i][0].toLowerCase() + categories[i].substring(1).replace(' ','')
+    const ratingDiv = document.createElement("div")
+    ratingDiv.className = 'rating-item'
+    //let rating = Math.floor(Math.random() * 10)
+    let rating = movieData['categories'][categories[i]]
+    overallTotal+= rating * weights[i]
+    overallOutOf+= 10 * weights[i]
+    ratingDiv.innerHTML = categoryName + ': ' + rating;
+    ratingDiv.style.borderColor = getProgressColor(rating*10)
+    rightDiv.appendChild(ratingDiv)
+  }
+
+  const overallDiv = document.createElement("div")
+  overallDiv.className = 'rating-item'
+  let rating = (overallTotal / overallOutOf) * 10
+  overallDiv.innerHTML =  'Overall: ' + rating.toFixed(2);;
+  overallDiv.style.borderColor = getProgressColor(rating*10)
+  rightDiv.appendChild(overallDiv)
+
+  return containerDiv
+}
--- rater-frontend/src/search.js
+++ rater-frontend/src/search.js
@@ -1,4 +1,4 @@
-var categories = ['Plot', 'Acting', 'Ending', 'Soundtrack', 'Cinematography', 'Family Friendy', 'Funny', 'Action']
+var categories = ['Plot', 'Acting', 'Ending', 'Soundtrack', 'Cinematography', 'Family Friendly', 'Funny', 'Action']
 
 window.onload = function () {
     var buttonList = document.getElementById('button-list')
@@ -6,49 +6,57 @@ window.onload = function () {
     for(var i = 0; i < categories.length; i++){
         var categoryButton = document.createElement('button')
         categoryButton.innerHTML = categories[i]
+        categoryButton.className = 'category-button'
         categoryButton.addEventListener("click", function(event) {
             var nav = document.getElementById('search-nav')
             const containerDiv = document.createElement("div")
             containerDiv.className = 'search-container'
-    
-            nav.appendChild(containerDiv)
-    
+
+            //nav.appendChild(containerDiv)
+
             const textContainerDiv = document.createElement("div")
             textContainerDiv.className = 'text-container'
-    
+
             const barContainerDiv = document.createElement("p")
             barContainerDiv.className = 'bar-container'
-    
+
             const text = document.createElement("label")
 
             text.innerHTML = event.target.innerHTML
-    
+
             const range = document.createElement("input")
             range.type = 'range'
             range.min = 0
             range.max = 4
             range.value = 2
-    
+
             const multiplier = document.createElement("label")
             var yv = '1.0x'
             multiplier.innerHTML = yv
-            
+
             range.addEventListener("input", (event) => {
-                multiplier_values = ['0.0x', '0.5x', '1.0x', '1.5x', '2.0x']
+                let multiplier_values = ['0.0x', '0.5x', '1.0x', '1.5x', '2.0x']
                 multiplier.innerHTML = multiplier_values[event.target.value]
-                
+
               })
-    
+
             textContainerDiv.appendChild(text)
             barContainerDiv.appendChild(range)
             barContainerDiv.appendChild(multiplier)
             containerDiv.appendChild(textContainerDiv)
             containerDiv.appendChild(barContainerDiv)
 
-            nav.appendChild(containerDiv)
+            var currentParameters = []
+            let parameterChildren = nav.children
+            for(let i = 0; i < parameterChildren.length; i++){
+              currentParameters.push(parameterChildren[i].children[0].children[0].innerHTML)
+            }
+            if(!currentParameters.includes(event.target.innerHTML)){
+              nav.appendChild(containerDiv)
+            }
         })
         buttonList.appendChild(categoryButton)
-        
+
       }
 
     var b = document.getElementById('button1')
