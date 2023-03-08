909f84937775c88a3b015807093d7220956329e7
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2023-03-07 21:29:55 -0500

    Refactoring

--- data-processing/src/averagesentiments.py
+++
@@ -1,5 +0,0 @@
-from review import Review
-import pandas as pd
-
-def average_sentiments(reviews: list[Review]):
-    df = pd.DataFrame(l)
--- data-processing/src/sentiments.py
+++ data-processing/src/sentiments.py
@@ -1,22 +1,19 @@
 from review import Review
-from averagesentiments import average_sentiments
 import pandas as pd
 import numpy as np
 import json
 from json_to_review import json_to_reviews
 
 # Keeps track of the sentiments extracted from reviews of a particular movie
-class Movie:
+class Sentiments:
     
     # Constructor for a known movie but no known reviews
-    def __init__(self, id) -> None:
-        self.sentiments = pd.DataFrame()
-        self.id = id
-
+    def __init__(self) -> None:
+        self.raw_sentiments = pd.DataFrame()
+        
     # Constructor for a set of reviews
     def __init__(self, reviews: list[Review]) -> None:
-        self.sentiments = pd.DataFrame()
-        self.id = reviews[0].movie_id
+        self.raw_sentiments = pd.DataFrame()
         self.add_reviews(reviews)
     
     # Adds the sentiments of multiple reviews
@@ -24,30 +21,30 @@ class Movie:
         l = [review.sentiments for review in reviews]
         df = pd.concat(l, axis=1).transpose()
         df["score"] = [int(review.score) for review in reviews]
-        self.sentiments = pd.concat([self.sentiments, df], ignore_index=True)
+        self.raw_sentiments = pd.concat([self.raw_sentiments, df], ignore_index=True)
 
     # Scores the movie
     def calculate_score(self):
         final_scores = {}
         # Take a weighted average of the scores based on the number of mentions in each review
-        for column in self.sentiments.drop(columns="score").columns:
+        for column in self.raw_sentiments.drop(columns="score").columns:
 
             # Default to 5 if no reviews include categories
-            if np.sum(self.sentiments[column]) == 0:
+            if np.sum(self.raw_sentiments[column]) == 0:
                 val = 5
             else:
-                val = round(np.average(self.sentiments["score"].to_numpy(), weights=self.sentiments[column].to_numpy()), 1)
+                val = round(np.average(self.raw_sentiments["score"].to_numpy(), weights=self.raw_sentiments[column].to_numpy()), 1)
             final_scores[column] = val
         self.scores = final_scores
 
     # Print a json string
     def to_json(self):
+      self.calculate_score()
       categories = {}
       for category in self.scores.keys():
           categories[category] = self.scores[category]
-      print(json.dumps(categories))  
+      return json.dumps(categories)
 
 
-s = Movie(json_to_reviews("rater-backend/backend/src/main/resources/static/movies/movie1.json"))
-s.calculate_score()
+s = Sentiments(json_to_reviews("rater-backend/backend/src/main/resources/static/movies/movie1.json"))
 s.to_json()
