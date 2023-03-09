edce6fa5fb969f6b190f85921c8fe69fd23e71f0
Andrew Lewandowski <48466309+andylewi@users.noreply.github.com>
2023-03-08 22:27:27 -0500

    Added more slider options and remove button

--- rater-frontend/src/search.js
+++ rater-frontend/src/search.js
@@ -1,6 +1,8 @@
 var categories = ['Plot', 'Acting', 'Ending', 'Soundtrack', 'Cinematography', 'Family Friendly', 'Funny', 'Action']
 
 window.onload = function () {
+    document.getElementById('tag-input').style.height="35px";
+    document.getElementById('tag-input').style.fontSize="16pt";
     var buttonList = document.getElementById('button-list')
 
     for(var i = 0; i < categories.length; i++){
@@ -14,11 +16,25 @@ window.onload = function () {
 
             //nav.appendChild(containerDiv)
 
+            
             const textContainerDiv = document.createElement("div")
             textContainerDiv.className = 'text-container'
 
             const barContainerDiv = document.createElement("p")
             barContainerDiv.className = 'bar-container'
+
+            const closeButtonContainer = document.createElement("p")
+            barContainerDiv.className = 'close-container'
+
+            const closeButton = document.createElement('button')
+            closeButton.className = 'close-button'
+            closeButton.innerHTML = 'remove'
+
+            closeButton.addEventListener("click", (event) => {
+              console.log('hi')
+              containerDiv.remove()
+
+            })
 
             const text = document.createElement("label")
 
@@ -27,15 +43,17 @@ window.onload = function () {
             const range = document.createElement("input")
             range.type = 'range'
             range.min = 0
-            range.max = 4
-            range.value = 2
+            range.max = 40
+            range.value = 20
 
             const multiplier = document.createElement("label")
             var yv = '0.0'
             multiplier.innerHTML = yv
-
+            let multiplier_values = []
+            for(let i = 0; i <= 40; i++){
+              multiplier_values.push(('' + (-1.0 + i * (2.0 / 40)).toFixed(2)))
+            }
             range.addEventListener("input", (event) => {
-                let multiplier_values = ['-1.0', '-0.5', '0.0', '0.5', '1.0']
                 multiplier.innerHTML = multiplier_values[event.target.value]
 
               })
@@ -43,8 +61,10 @@ window.onload = function () {
             textContainerDiv.appendChild(text)
             barContainerDiv.appendChild(range)
             barContainerDiv.appendChild(multiplier)
+            closeButtonContainer.appendChild(closeButton)
             containerDiv.appendChild(textContainerDiv)
             containerDiv.appendChild(barContainerDiv)
+            containerDiv.appendChild(closeButtonContainer)
 
             var currentParameters = []
             let parameterChildren = nav.children
