6aff9803e9635272156b14f0e401871070348e83
JacobSulewski <Sulewski2001@gmail.com>
2023-03-09 00:14:46 -0500

    Script score query

--- rater-backend/backend/src/main/java/rater/application/search/util/SearchUtil.java
+++ rater-backend/backend/src/main/java/rater/application/search/util/SearchUtil.java
@@ -1,5 +1,6 @@
 package rater.application.search.util;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +11,9 @@ import org.elasticsearch.common.lucene.search.function.FunctionScoreQuery.ScoreM
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
 import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
+import org.elasticsearch.index.query.functionscore.ScriptScoreQueryBuilder;
 import org.elasticsearch.script.Script;
+import org.elasticsearch.script.ScriptType;
 import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.SortBuilders;
@@ -49,50 +52,28 @@ public class SearchUtil {
         }
     }
 
-    public static SearchRequest buildSearchRequest(final String indexName, Map<String, Float> categoriesBoost) {
 
+    
+    public static SearchRequest buildSearchRequest(final String indexName, Map<String, Float> categoriesBoost) {
         try {
+            String code = 
+                "double score = 100000.0;" +
+                "for (def category : params.categories.keySet()) {" +
+                "    score += doc['categories.' + category].value * params.categories[category];" +
+                "}" +
+                "return score;"; 
+            Map<String, Object> params = Collections.singletonMap("categories", categoriesBoost);
+            // create the function score query  
+            Script script = new Script(ScriptType.INLINE, "painless", code, params);
+            ScriptScoreQueryBuilder scriptScoreQueryBuilder = QueryBuilders.scriptScoreQuery(QueryBuilders.matchAllQuery(), script);
+
+            // add additional query parameters as needed
+            // ...
+
+            // create the search request and return it
             SearchRequest searchRequest = new SearchRequest(indexName);
             SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        
-            final BoolQueryBuilder queryBuilder = new BoolQueryBuilder();
-
-            for (Map.Entry<String, Float> category : categoriesBoost.entrySet()) {
-                String fieldName = "categories." + category.getKey().toLowerCase(); //gets "categories.categoryName", the name of the category in the json doc
-                if (fieldName.equals("categories.familyfriendly")) {
-                    fieldName = "categories.familyFriendly";
-                }
-                RangeQueryBuilder rangeQuery = new RangeQueryBuilder(fieldName);
-                Float boost = category.getValue();
-                if (boost == 0) {
-                    continue;
-                }
-                if (boost < 0) {
-                    if (boost < -0.5) {
-                        queryBuilder.should(new RangeQueryBuilder(fieldName).lte(4));
-                        queryBuilder.should(new RangeQueryBuilder(fieldName).lte(3));
-                        queryBuilder.should(new RangeQueryBuilder(fieldName).lte(2));
-                    }
-                    queryBuilder.should(new RangeQueryBuilder(fieldName).lte(5));
-                    queryBuilder.should(new RangeQueryBuilder(fieldName).lte(6));
-                } else {
-                    if (boost > 0.5) {
-                        queryBuilder.should(new RangeQueryBuilder(fieldName).gte(9));
-                        queryBuilder.must(new RangeQueryBuilder(fieldName).gte(8));
-                    } 
-                    queryBuilder.must(new RangeQueryBuilder(fieldName).gte(7));
-                }
-                //TODO: right now, we don't want to use category.value since that will only be 0.5, 1, or 1.5 (most movies will be above that range)
-                //in the future, we want to fine tune the gte value, but for now, if the category is listed, we will assume just make it gte 7
-                //queryBuilder.should(rangeQuery);
-            }
-        
-            // set query and sort
-            searchSourceBuilder.query(queryBuilder);
-            searchSourceBuilder.sort(SortBuilders.scoreSort().order(SortOrder.DESC));
-            searchSourceBuilder.from(0);
-            searchSourceBuilder.size(10);
-        
+            searchSourceBuilder.query(scriptScoreQueryBuilder);
             searchRequest.source(searchSourceBuilder);
             return searchRequest;
         } catch (final Exception e) {
