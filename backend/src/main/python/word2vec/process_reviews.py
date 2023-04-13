1c28bcf9ec30cc39031a9f806b41c2a1879fad66
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2023-04-13 17:09:19 -0400

    Added embedding for movies

--- backend/src/main/python/word2vec/process_reviews.py
+++ backend/src/main/python/word2vec/process_reviews.py
@@ -1,5 +1,6 @@
-from gensim.parsing.preprocessing import preprocess_string
+from gensim.parsing.preprocessing import preprocess_string, STOPWORDS
 from gensim.models import Word2Vec, KeyedVectors
+import json
 import numpy as np
 import pandas as pd
 import logging
@@ -7,7 +8,31 @@ import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 logging.info("Program Start")
 
-vectors = KeyedVectors.load_word2vec_format("backend/src/main/python/word2vec/model/vectors.kv")
+class embedder:
 
-word = preprocess_string("acting")
+    def __init__(self, vector_embeddings: str) -> None:
+        self.wv = KeyedVectors.load_word2vec_format("backend/src/main/python/word2vec/model/vectors.kv")
+
+    def embed_movie(self, movie_file: str):
+        movie = json.load(open(movie_file))
+        embedding = np.zeros(100)
+        for review in movie["reviews"]:
+            text = review["text"]
+            embedding += self.__embed_review(text)
+        print(self.wv.most_similar(positive=embedding))
+    
+    def __embed_review(self, review: str):
+        review = preprocess_string(review)
+        embedding = np.zeros(100)
+        for word in review:
+            if self.wv.has_index_for(word):
+                embedding += self.wv.get_vector(word)
+        return embedding
+
+
+e = embedder("backend/src/main/python/word2vec/model/vectors.kv")
+e.embed_movie("backend/src/main/python/documents/101_dalmatians.json")
+"""
+word = preprocess_string("cool")
 print(vectors.most_similar(positive=word))
+"""
